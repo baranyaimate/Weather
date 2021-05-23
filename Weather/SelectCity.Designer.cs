509848a6ce0acf03c6566e4cae199f7d29c80e1c
Baranyai Máté <mateb06@gmail.com>
2021-05-23 11:58:43 +0200

    Init

---
+++ Weather/SelectCity.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Quicksand", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1170, 75);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select city";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(471, 153);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(262, 460);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1194, 671);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Weather";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.ListBox listBox1;
+
+        private System.Windows.Forms.Label label2;
+
+        #endregion
+    }
+}
---
+++ Weather/SelectCity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Weather
+{
+    public partial class Form1 : Form
+    {
+        static readonly HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            //var responseString = await client.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=pécs,hu&APPID=0079a590cd56c31ffb2f4105946aeb6f");
+            
+            //label1.Text = responseString;
+            LoadCities();
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
+                Color.FromArgb(66, 134, 244),
+                Color.FromArgb(55, 59, 68),
+                90F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private async void LoadCities()
+        {
+            StreamReader r = new StreamReader("C:/Users/mateb/Downloads/city.list.min.json");
+            string jsonString = r.ReadToEnd();
+            List<City> cityList = JsonConvert.DeserializeObject<List<City>>(jsonString);
+            for (int i = 0; i < cityList.Count; i++)
+            {
+                listBox1.Items.Add(cityList[i]);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(e);
+        }
+    }
+    
+    public class City
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
